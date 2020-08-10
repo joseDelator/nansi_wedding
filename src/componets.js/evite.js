bbd014b143a08dc12cfcb56a00c3f0e0f4dc6f04
joseDelator <49530849+joseDelator@users.noreply.github.com>
2020-08-10 12:15:02 -0700

    new

---
+++ src/componets.js/evite.js
@@ -0,0 +1,31 @@
+import React, { Component} from 'react';
+import {Link } from 'react-router-dom';
+import Lotus from './Lotussvg'
+import Bottom_Left from './bottomleft'
+import Top_Right from './topright'
+import './styleevite.css'
+class Evite extends Component{
+
+    render(){return(
+        <div className='evite-contianer'>
+            <Lotus />
+            <Bottom_Left />
+            <Top_Right />
+            <div className='words'>
+                <h3>Nansi De La Torre</h3>
+                <h5>&</h5>
+                <h3>Eder something</h3>
+                <h3>Wedding</h3>
+                <div className='info'>
+                    <div className="box"><h4>lcoations</h4><p></p></div>
+                    <div className="box"><h4>date</h4><p>aug</p><h1>24</h1></div>
+                    <div className="box"><h4>lcoations</h4><p></p></div>
+                </div>
+            </div>
+             
+        </div>
+    )
+
+    }
+}
+export default Evite
